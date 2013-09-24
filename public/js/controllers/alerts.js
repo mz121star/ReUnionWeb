c1da4d7b8de47b682e26bc6a60de29517ada2b1d
mz121star <mz121star@gmail.com>
2013-09-24 13:14:47 +0800

    new

--- public/js/controllers/alerts.js
+++ public/js/controllers/alerts.js
@@ -4,7 +4,19 @@ define([ 'i18n!resources/nls/res'], function (res) {
 
     var  AlertsController=['$scope','$rootScope',function($scope,$rootScope) {
         $rootScope.title = "Alerts - "+ res.title;
+        $scope.definefrom = ['Topics', 'Fresh New' ];
+        $scope.choosetopics = ['topic1', 'topic2','topic3'];
+        $scope.sourcetype = ['News', 'Forum','eCommerce','Weibo','Sohu','baidu'];
+        $scope.showsection1 = function() {
 
+            $scope.show1=true;
+            $scope.show2=false;
+        };
+        $scope.showsection2 = function() {
+
+            $scope.show1=false;
+            $scope.show2=true;
+        };
     }];
 
     return AlertsController;
