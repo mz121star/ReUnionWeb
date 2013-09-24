2a4236e200d517a22be4bd696cafd3bc699d44bf
mz121star <mz121star@gmail.com>
2013-09-24 14:24:16 +0800

    fixed radio button issues

--- public/js/controllers/alerts.js
+++ public/js/controllers/alerts.js
@@ -7,16 +7,7 @@ define([ 'i18n!resources/nls/res'], function (res) {
         $scope.definefrom = ['Topics', 'Fresh New' ];
         $scope.choosetopics = ['topic1', 'topic2','topic3'];
         $scope.sourcetype = ['News', 'Forum','eCommerce','Weibo','Sohu','baidu'];
-        $scope.showsection1 = function() {
 
-            $scope.show1=true;
-            $scope.show2=false;
-        };
-        $scope.showsection2 = function() {
-
-            $scope.show1=false;
-            $scope.show2=true;
-        };
     }];
 
     return AlertsController;
