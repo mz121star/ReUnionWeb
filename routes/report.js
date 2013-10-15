63d15e8468fc797a220ad95607efc4473a758813
mz121star <mz121star@gmail.com>
2013-10-15 13:56:55 +0800

    language- by jarrick

--- routes/report.js
+++ routes/report.js
@@ -644,8 +644,8 @@ exports.SentimentAnalysisPost = function (req, res) {
                         for( d in badResult){
                             pl.bad.push(badResult[d].value) ;
                         }
-                        finalResult.push({name: "好评", value: pl.good, color: "#4572a7", line_width: 2});
-                        finalResult.push({name: "差评", value: pl.bad, color:"#aa4643", line_width: 2})
+                        finalResult.push({name: "Good", value: pl.good, color: "#4572a7", line_width: 2});
+                        finalResult.push({name: "Bad", value: pl.bad, color:"#aa4643", line_width: 2})
                         var result = {data: finalResult, labels: utils.dateRange(params.starttime,params.endtime)}
                         return res.json(result);
                         /*结束*/
@@ -810,8 +810,8 @@ exports.SentimentAnalysisColumn = function (req, res) {
                     }
 
                 }
-                finalResult.push({name: "好评", value: pl.good, color: utils.randomColor(), line_width: 2});
-                finalResult.push({name: "差评", value: pl.bad, color: utils.randomColor(), line_width: 2})
+                finalResult.push({name: "Good", value: pl.good, color: utils.randomColor(), line_width: 2});
+                finalResult.push({name: "Bad", value: pl.bad, color: utils.randomColor(), line_width: 2})
                 //                    处理结束
                 return res.json(finalResult);
             });
@@ -859,8 +859,8 @@ exports.SentimentAnalysisColumnPost = function (req, res) {
                         for( d in badResult){
                             pl.bad.push(badResult[d].value) ;
                         }
-                        finalResult.push({name: "好评", value: pl.good, color:"#4572a7", line_width: 2});
-                        finalResult.push({name: "差评", value: pl.bad, color: "#aa4643", line_width: 2})
+                        finalResult.push({name: "Good", value: pl.good, color:"#4572a7", line_width: 2});
+                        finalResult.push({name: "Bad", value: pl.bad, color: "#aa4643", line_width: 2})
                         var result = {data: finalResult, labels: utils.dateRange(params.starttime,params.endtime)}
                         return res.json(result);
                         /*结束*/
