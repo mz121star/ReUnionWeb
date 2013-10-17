671f677e17bfdb3afedf17a074dc5aa008716763
mz121star <mz121star@gmail.com>
2013-10-17 13:12:25 +0800

    关于语义分析报表函数时间有空白的问题，写了个函数进行补齐

--- routes/report.js
+++ routes/report.js
@@ -357,141 +357,101 @@ exports.SentimentAnalysisColumn = function (req, res) {
             });
     });
 };
-exports.SentimentAnalysisColumnPost = function (req, res) {
+exports.SentimentAnalysisPost = function (req, res) {
     var params = req.body;
     var startDate = new Date(params.starttime) , endDate = new Date(params.endtime);
+    /*added by miaozhuang*/
+    endDate = utils.getEndDate(startDate, endDate, 30);
+    /*End*/
     FeedsModel.aggregate(
         { $match: { PublishTimeTemp: { $gte: new Date(startDate), $lte: new Date(endDate)}, Semantic: {$gt: 0} }},
         { $group: { _id: "$PublishTimeTemp", value: { $sum: 1 }} },
         { $project: {name: "$_id", value: 1 }},
         { $sort: { name: 1 } },
         function (err, docs) {
-            var goodResult = [],badResult=[];
+            var goodResult = [], badResult = [], normalResult = [];
+            //TODO 此处循环需要替换，替换为补齐时间的函数  ，参考docs2内的     /*miaozhuang add*/起始内容
             for (var d in docs) {
                 docs[d].color = utils.randomColor();
                 goodResult.push(docs[d]);
+            }
+            FeedsModel.aggregate(
+                { $match: { PublishTimeTemp: { $gte: new Date(startDate), $lte: new Date(endDate)}, Semantic: {$lt: 0} }},
+                { $group: { _id: "$PublishTimeTemp", value: { $sum: 1 }} },
+                { $project: {name: "$_id", value: 1 }},
+                { $sort: { name: 1 } },
+                function (err, docs2) {
+                    //TODO 此处循环需要替换，替换为补齐时间的函数
+                    for (var d2 in docs2) {
+                        docs2[d2].color = utils.randomColor();
+                        badResult.push(docs2[d2]);
+                    }
 
-                /***
-                 *second
-                 */
-                FeedsModel.aggregate(
-                    { $match: { PublishTimeTemp: { $gte: new Date(startDate), $lte: new Date(endDate)}, Semantic: {$lt: 0} }},
-                    { $group: { _id: "$PublishTimeTemp", value: { $sum: 1 }} },
-                    { $project: {name: "$_id", value: 1 }},
-                    { $sort: { name: 1 } },
-                    function (err, docs2) {
-
-                        for (var d2 in docs2) {
-                            docs2[d2].color = utils.randomColor();
+                    /*miaozhuang
+                     * 此段内容为模板文件，会自动将有丢失的日期补齐，并自动将其中的语义分析value设置为0
+                     * */
+                    badResult = [];
+                    for (var d2 in docs2) {
+                        /*  console.log(d2);
+                         console.log(docs2[parseInt(d2) + 1]);*/
+                        docs2[d2].color = "#fff";
+                        if (docs2[parseInt(d2) + 1]) {
+                            var dd = moment(docs2[parseInt(d2) + 1].name).diff(moment(docs2[d2].name), 'days'); //下一个日期和当前日期差几天，大于1天说明中间有空数据，需要补齐
+                            console.log("相差时间"+dd);
+                            if (dd > 1) { //有空数据，需要进行补齐操作
+                                for (var i=0; i < dd; i++) { //dd为差的天数，循环此天数，以当前日期为基数，开始累加
+                                    badResult.push({name: moment(docs2[d2].name).add("days", i).format("YYYY-MM-DD"), value: 0, color: docs2[d2].color});    //向数组中push时间，format中为要格式化的时间格式
+                                }
+                            }
+                            else {
+                                badResult.push(docs2[d2]);
+                            }
+                        }
+                        else{
                             badResult.push(docs2[d2]);
                         }
+                    }
+                    console.log(badResult)
+                    /*模板结束*/
+                    FeedsModel.aggregate(
+                        { $match: { PublishTimeTemp: { $gte: new Date(startDate), $lte: new Date(endDate)}, Semantic: 0 }},
+                        { $group: { _id: "$PublishTimeTemp", value: { $sum: 1 }} },
+                        { $project: {name: "$_id", value: 1 }},
+                        { $sort: { name: 1 } },
+                        function (err, docs3) {
+                            //TODO 此处循环需要替换，替换为补齐时间的函数
+                            for (var d3 in docs3) {
+                                docs3[d3].color = utils.randomColor();
+                                normalResult.push(docs3[d3]);
+                            }
 
-                        /**处理*/
-                        var finalResult = [];
-                        var pl = {
-                            good: [],
-                            normal: [],
-                            bad: []
-                        }
-                        for( var g in goodResult){
-                            pl.good.push(goodResult[g].value) ;
-                        }
-                        for( var b in badResult){
-                            pl.bad.push(badResult[b].value) ;
-                        }
-                        finalResult.push({name: "Positive", value: pl.good, color:"#4572a7", line_width: 2});
-                        finalResult.push({name: "Negative", value: pl.bad, color: "#aa4643", line_width: 2})
-                        var result = {data: finalResult, labels: utils.dateRange(params.starttime,params.endtime)}
-                        return res.json(result);
-                        /*结束*/
-                    });
-            }
+                            /**处理*/
+                            var finalResult = [];
+                            var pl = {
+                                good: [],
+                                normal: [],
+                                bad: []
+                            }
+                            for (var g in goodResult) {
+                                pl.good.push(goodResult[g].value);
+                            }
+                            for (var b in badResult) {
+                                pl.bad.push(badResult[b].value);
+                            }
+                            for (var n in normalResult) {
+                                pl.normal.push(normalResult[n].value);
+                            }
+                            finalResult.push({name: "Positive", value: pl.good, color: "#4572a7", line_width: 2});
+                            finalResult.push({name: "Neutral", value: pl.normal, color: "#959700", line_width: 2});
+                            finalResult.push({name: "Negative", value: pl.bad, color: "#aa4643", line_width: 2})
+                            var result = {data: finalResult, labels: utils.dateRange(params.starttime, params.endtime)}
+                            return res.json(result);
+                            /*结束*/
+                        });
+                });
+
         });
-//    var params = req.body
-//    var o = {};
-//
-//    o.map = function () {
-//        if (this.PublishTime && this.PublishTime.getFullYear() === 2013) {
-//            //var key = this.PublishTime..match(/-(\d*)/)[1];
-//            var month = this["PublishTime"].getMonth() + 1;
-//            var day = this["PublishTime"].getDate();
-//            if (month < 10) {
-//                month = "0" + month;
-//            }
-//            if (day < 10) {
-//                day = "0" + day;
-//            }
-//            var key = this["PublishTime"].getFullYear() + "-" + month + "-" + day;
-//            if (this.Semantic > 0) {
-//                emit({s: "好评", d: key}, 1);
-//            }
-//            if (this.Semantic < 0) {
-//                emit({s: "差评", d: key}, 1);
-//            }
-//        }
-//
-//    }
-//
-//    o.reduce = function (k, vals) {
-//        var total = 0;
-//        for (var i in vals) {
-//            total += vals[i];
-//        }
-//        return total;
-//
-//    };
-//
-//
-//    o.finalize = function (k, reduced) {
-//
-//        return {name: k, value: reduced}
-//    };
-//
-//    o.out = { replace: 'SentimentAnalysisReportColumnForResultsPost' };
-//    o.verbose = true;
-//    FeedsModel.mapReduce(o, function (err, model, stats) {
-//        if (err) {
-//            return res.json(500, err);
-//        }
-//        model.find().sort({PublishTime: 'desc'}).select("value")
-//            //*.where('value').gt(10)*//*
-//
-//            .exec(function (err, docs) {
-//                if (!!!docs.length || err) {
-//                    return res.json(500, docs);
-//                }
-//                var result = [];
-//                var temp1 = [], temp2 = [], temp3 = [];
-//                var startDate = new Date(params.starttime) , endDate = new Date(params.endtime);
-//                for (var dl in docs) {
-//                    if (new Date(docs[dl].value.name.d) >= startDate && new Date(docs[dl].value.name.d) <= endDate) {
-//                        docs[dl].value.color = utils.randomColor();
-//                        result.push(docs[dl].value);
-//                    }
-//                }
-//                //处理数据
-//                var finalResult = [];
-//                var pl = {
-//                    good: [],
-//                    normal: [],
-//                    bad: []
-//                }
-//                for (var i in result) {
-//                    var v = result[i];
-//                    if (v.name.s === "好评") {
-//                        pl.good.push(v.value);
-//                    }
-//                    if (v.name.s === "差评") {
-//                        pl.bad.push(v.value);
-//                    }
-//                }
-//                finalResult.push({name: "好评", value: pl.good, color: utils.randomColor(), line_width: 2});
-//                finalResult.push({name: "差评", value: pl.bad, color: utils.randomColor(), line_width: 2})
-//                //                    处理结束
-//                var result = {data: finalResult, labels: utils.dateRange(params.starttime, params.endtime)}
-//                return res.json(result);
-//            });
-//    });
+
 };
 exports.SentimentAnalysisByFromTypeBarPost = function (req, res) {
     var params = req.body;
