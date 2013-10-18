1151e3e2ab29129b812600bc901da88661cfd191
mz121star <mz121star@gmail.com>
2013-10-18 17:55:46 +0800

    增加稳定性- by jarrick

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,6 @@
 var path = require('path');
 exports.index = function (req, res) {
 
-//    var jarrick = new Users({name:'Jarrick' + Math.random() * 1000, sex:'0', phone:'12345678901',
-//        address:{city:'dalian', 'street':'xigang', test:'test'}});
-//    jarrick.save(function (data) {
-//        console.log(data);
-//    });
-
-//    Users.getById('51037e2a8a75b31c53000001');
-//    Users.getByName('Jarrick678.9289640728384');
     var s =req.session["user"] ;
     if(!s){
         res.redirect('login');
@@ -21,7 +13,7 @@ exports.index = function (req, res) {
     }
     var html = path.normalize(__dirname + '/../views/index.html');
     res.sendfile(html);
-//    res.render('index', { title:'NJBlog.' });
+
 };
 
 exports.login = function (req, res) {
