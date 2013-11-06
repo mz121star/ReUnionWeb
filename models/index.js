06dd0fbaa38503956b63faca685f04e7939f48ab
mz121star <mz121star@gmail.com>
2013-11-06 16:13:51 +0800

    js

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ var log = require('./../libs/log');
 mongoose.connect(config.connectionstring);
 
 var db = mongoose.connection;
-db.on('error', function(err){
+db.on('error', function (err) {
     console.error('connect to %s error: ', config.connectionstring, err.message);
     process.exit(1);
 });
@@ -16,7 +16,9 @@ db.once('open', function () {
 
 var models_path = __dirname + '/../models/mapping'
 fs.readdirSync(models_path).forEach(function (file) {
-    require(models_path + '/' + file);
-    var modelName = file.replace('Model.js', '');
-    exports[modelName] = mongoose.model(modelName);
+    if (file.match(/.js/)) {
+        require(models_path + '/' + file);
+        var modelName = file.replace('Model.js', '');
+        exports[modelName] = mongoose.model(modelName);
+    }
 });
